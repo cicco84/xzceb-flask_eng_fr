027f58cf6aafcdc962993a731106d4b06cfb2a80
Marco Cicchinelli <marco.cicchinelli@telespazio.com>
2022-11-14 12:07:15 +0100

    bug fixed

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -10,9 +10,11 @@ url = os.environ['url']
 version_lt = '2018-05-01'
 def englishToFrench(englishText):
     eng_french_translator = LanguageTranslatorV3(iam_apikey=apikey, url=url, version=version_lt)
-    return (eng_french_translator.translate(text=englishText),model_id='en-fr')
+    return (eng_french_translator.translate(text=englishText,model_id='en-fr'))
 
 def frenchToEnglish(frenchText):
     french_eng_translator = LanguageTranslatorV3(iam_apikey=apikey, url=url, version=version_lt)
-    return (french_eng_translator.translate(text=frenchText),model_id='fr-en')
+    return (french_eng_translator.translate(text=frenchText,model_id='fr-en'))
 
+if __name__ == "__main__":
+    print(englishToFrench("Hello"))
