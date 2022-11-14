eb976e67b47218ad411f2c272e6729df0e5e7ba9
Marco Cicchinelli <marco.cicchinelli@telespazio.com>
2022-11-14 12:15:01 +0100

    auth fix

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,12 +8,14 @@ load_dotenv()
 apikey=os.environ['apikey']
 url = os.environ['url']
 version_lt = '2018-05-01'
+
+authenticator  = IAMAuthenticator(apikey)
 def englishToFrench(englishText):
-    eng_french_translator = LanguageTranslatorV3(iam_apikey=apikey, url=url, version=version_lt)
+    eng_french_translator = LanguageTranslatorV3(authenticator=authenticator, url=url, version=version_lt)
     return (eng_french_translator.translate(text=englishText,model_id='en-fr'))
 
 def frenchToEnglish(frenchText):
-    french_eng_translator = LanguageTranslatorV3(iam_apikey=apikey, url=url, version=version_lt)
+    french_eng_translator = LanguageTranslatorV3(authenticator=authenticator, url=url, version=version_lt)
     return (french_eng_translator.translate(text=frenchText,model_id='fr-en'))
 
 if __name__ == "__main__":
